5ff83675204558dd326625b3854740e5968eb56d
Ollie Pai <ollie.pai2003@gmail.com>
2017-05-12 16:49:12 -0700

    city

---
+++ src/robot.java
@@ -0,0 +1,271 @@
+import org.jointheleague.graphical.robot.Robot;
+
+public class robot {
+
+	static Robot bin = new Robot();
+
+	public static void main(String[] args) {
+		bin.setX(10);
+		bin.setY(900);
+		bin.setSpeed(100);
+		bin.setPenWidth(10);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.move(100);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(100);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(190);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(190);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(180);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(180);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(90);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(90);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(80);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(80);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(120);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(120);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(190);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(190);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(180);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(180);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(90);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(90);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(190);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(190);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(180);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(180);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(90);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(90);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		/// taller
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(220);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(220);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(300);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(300);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// tent
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(190);
+		bin.turn(45);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(45);
+		bin.move(190);
+		bin.turn(-90);
+		bin.move(20);
+		bin.setRandomPenColor();
+		// box
+		bin.penDown();
+		bin.turn(-90);
+		bin.move(170);
+		bin.turn(90);
+		bin.move(50);
+		bin.turn(90);
+		bin.move(170);
+		bin.turn(-90);
+		bin.move(20);
+
+	}
+}
